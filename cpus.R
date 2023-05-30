b7295174cbe83a462e053734bbbef70326cb7386
Dabowski <ukrytytomek@gmail.pl>
2023-05-30 23:46:58 +0200

    Dashboard skeleton

--- cpus.R
+++ cpus.R
@@ -1,44 +1,72 @@
 library(tidyverse)
+library(shiny)
 
 # Smartphone Processors Ranking & Scores
 
 cpus <- read_csv("ML_ALL_benchmarks.csv")
-head(cpus)
 
-# Top CPU Performance
-cpus %>%
-    arrange(desc(cpuScore)) %>%
-    head(10)
+ui <- fluidPage(
+    tableOutput("cpuTable"),
+    tableOutput("gpuTable"),
+    tableOutput("tpuTable"),
+    tableOutput("clockDist"),
+    tableOutput("coresDist"),
+    tableOutput("perfomance")
+)
 
-# Top GPU Performance
-cpus %>%
-    arrange(desc(gpuScore)) %>%
-    head(10)
+server <- function(input, output, session){
 
-# Top TPU Performance
-cpus %>%
-    arrange(desc(npuScore)) %>%
-    head(10)
+    output$cpuTable <- renderTable(
 
-# Clock Speed Distribution
-ggplot(cpus, mapping = aes(x = clock)) +
-    geom_histogram(binwidth = 500) +
-    xlab("Clock speed") +
-    ggtitle("Clock speed distribution")
+        # Top CPU Performance
+        cpus %>%
+            arrange(desc(cpuScore)) %>%
+            head(10)
+    )
 
-# Cores Distribution
-ggplot(cpus, mapping = aes(x = cores)) +
-    geom_histogram(binwidth = 1) +
-    xlab("Number of Cores") +
-    ggtitle("Core count distribution")
+    output$gpuTable <- renderTable(
 
-# Top Performance by Producent
-performance_pivot <- cpus %>%
-    group_by(company) %>%
-    summarize(averageCPUPerformance = mean(cpuScore),
-              averageGPUPerformance = mean(gpuScore),
-              averageNPUPerformance = mean(npuScore))
+        # Top GPU Performance
+        cpus %>%
+            arrange(desc(gpuScore)) %>%
+            head(10)
+    )
 
-performance_pivot
+    output$tpuTable <- renderTable(
 
-# Price predictions
+        # Top TPU Performance
+        cpus %>%
+            arrange(desc(npuScore)) %>%
+            head(10)
+    )
+
+    output$clockDist <- renderPlot(
+
+        # Clock Speed Distribution
+        ggplot(cpus, mapping = aes(x = clock)) +
+            geom_histogram(binwidth = 500) +
+            xlab("Clock speed") +
+            ggtitle("Clock speed distribution")
+    )
+
+    output$perfomance <- renderTable(
+
+        # Top Performance by Producent
+        cpus %>%
+            group_by(company) %>%
+            summarize(averageCPUPerformance = mean(cpuScore),
+                      averageGPUPerformance = mean(gpuScore),
+                      averageNPUPerformance = mean(npuScore))
+    )
+
+    output$coresDist <- renderPlot(
+
+        # Cores Distribution
+        ggplot(cpus, mapping = aes(x = cores)) +
+            geom_histogram(binwidth = 1) +
+            xlab("Number of Cores") +
+            ggtitle("Core count distribution")
+    )
+}
+
+shinyApp(ui, server)
